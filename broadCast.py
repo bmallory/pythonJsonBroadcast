9c0d0e4bdc522b4254a905428b48f8cc63fa9d1d
Cyril Cheney <Cyril Cheney>
2015-12-26 18:33:26 +0100

    ---

--- broadCast.py
+++ broadCast.py
@@ -18,7 +18,7 @@ import serial
 
 # configure the serial connections (the parameters differs on the device you are connecting to)
 ser = serial.Serial(
-    port='/dev/ttyUSB1',
+    port='/dev/ttyUSB0',
     baudrate=9600,
     parity=serial.PARITY_ODD,
     stopbits=serial.STOPBITS_TWO,
