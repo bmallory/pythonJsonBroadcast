60dcabff9a11d7388ff8cf7383e16a1d5e7392e6
Cyril Cheney <Cyril Cheney>
2015-12-26 17:48:13 +0100

    changement pour utilisation du port série

--- broadCast.py
+++ broadCast.py
@@ -1,14 +1,52 @@
-import http.client
-import json
+# import http.client
+# import json
 
-connection = http.client.HTTPSConnection('api.github.com')
+# connection = http.client.HTTPSConnection('api.github.com')
 
-headers = {'Content-type': 'application/json'}
+# headers = {'Content-type': 'application/json'}
 
-foo = {'text': 'Hello world github/linguist#1 **cool**, and #1!'}
-json_foo = json.dumps(foo)
+# foo = {'text': 'Hello world github/linguist#1 **cool**, and #1!'}
+# json_foo = json.dumps(foo)
 
-connection.request('POST', '/markdown', json_foo, headers)
+# connection.request('POST', '/markdown', json_foo, headers)
 
-response = connection.getresponse()
-print(response.read().decode())
+# response = connection.getresponse()
+# print(response.read().decode())
+
+import time
+import serial
+
+# configure the serial connections (the parameters differs on the device you are connecting to)
+ser = serial.Serial(
+    port='/dev/ttyUSB1',
+    baudrate=9600,
+    parity=serial.PARITY_ODD,
+    stopbits=serial.STOPBITS_TWO,
+    bytesize=serial.SEVENBITS
+)
+
+ser.isOpen()
+
+print 'Enter your commands below.\r\nInsert "exit" to leave the application.'
+
+input=1
+while 1 :
+    # get keyboard input
+    input = raw_input(">> ")
+        # Python 3 users
+        # input = input(">> ")
+    if input == 'exit':
+        ser.close()
+        exit()
+    else:
+        # send the character to the device
+        # (note that I happend a \r\n carriage return and line feed to the characters - this is requested by my device)
+        ser.write(input + '\r\n')
+        out = ''
+        # let's wait one second before reading output (let's give device time to answer)
+        time.sleep(1)
+        while ser.inWaiting() > 0:
+            out += ser.read(1)
+
+        if out != '':
+            print ">>" + out
