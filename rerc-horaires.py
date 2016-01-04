1b3ab7ccc6d691ce474be7459ecb50e6e0956bda
Cyril Cheney <bmallory@gmail.com>
2016-01-04 12:50:35 +0100

    api sncf

--- rerc-horaires.py
+++ rerc-horaires.py
@@ -1,4 +1,4 @@
-from urllib2 import urlopen
+import urllib2, base64
 import bs4 as BeautifulSoup
 import serial
 import sys
@@ -7,10 +7,17 @@ import unicodedata
 serialPort = '/dev/ttyUSB0'
 toSend = '{lcd1:"Prochain RER C:'
 
-html = urlopen('http://www.transilien.mobi/train/result?idOrigin=CHV&idDest=CPM').read()
-soup = BeautifulSoup.BeautifulSoup(html, "html5lib")
-for tag in soup.find_all(class_='heure_train'):
-	toSend += ' '+ tag.string[0:5]
+#html = urlopen('http://www.transilien.mobi/train/result?idOrigin=CHV&idDest=CPM').read()
+
+request = urllib2.Request('http://api.transilien.com/gare/87393173/depart/87393058')
+base64string = base64.encodestring('%s:%s' % ("username", "password")).replace('\n', '')
+request.add_header("Authorization", "Basic %s" % base64string)   
+xml = urllib2.urlopen(request)
+
+soup = BeautifulSoup.BeautifulSoup(xml, "xml")
+#for tag in soup.find_all(class_='heure_train'):
+for tag in soup.find_all("date"):
+	toSend += ' '+ tag.string[11:5]
 toSend += '"}'
 
 print(toSend)
