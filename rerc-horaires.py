3a3d7ff4e130f87bc869d98e52a535c6af070d97
bmallory <bmallory@gmail.com>
2016-01-04 13:04:08 +0100

    api sncf: ajout de l'etat des trains

--- rerc-horaires.py
+++ rerc-horaires.py
@@ -10,14 +10,17 @@ toSend = '{lcd1:"Prochain RER C:'
 #html = urlopen('http://www.transilien.mobi/train/result?idOrigin=CHV&idDest=CPM').read()
 
 request = urllib2.Request('http://api.transilien.com/gare/87393173/depart/87393058')
-base64string = base64.encodestring('%s:%s' % ("username", "password")).replace('\n', '')
+base64string = base64.encodestring('%s:%s' % ("XXX", "XXXXX")).replace('\n', '')
 request.add_header("Authorization", "Basic %s" % base64string)   
-xml = urllib2.urlopen(request)
+xml = urllib2.urlopen(request).read()
 
 soup = BeautifulSoup.BeautifulSoup(xml, "xml")
 #for tag in soup.find_all(class_='heure_train'):
-for tag in soup.find_all("date"):
-	toSend += ' '+ tag.string[11:5]
+for train in soup.find_all("train"):
+	for date in train.find_all("date"):
+		toSend += ' '+ date.string[11:16]
+	for etat in train.find_all("etat"):
+		toSend += etat.string[0:1]
 toSend += '"}'
 
 print(toSend)
